a12786054121352f51484dec1f2a93c9e56a7e19
Victor Okpara <victorjnr#300@gmail.com>
2022-11-05 20:17:53 +0100

    updated a custom hook

--- src/components/hooks/useFetch.jsx
+++ src/components/hooks/useFetch.jsx
@@ -1,13 +1,14 @@
-import  React, { useState, useEffect}  from "react";
+import { useState, useEffect}  from "react";
 
 const useFetch = (url, setLoading) => {
     const [data, setData] = useState(null);
   
-    useEffect(() => {
+    useEffect((setLoading) => {
       fetch(url)
         .then((res) => res.json())
-        .then((data) => {setData(data)
-        setLoading(false)});
+        .then((data) => {
+          setData(data)
+          setLoading(false)});
     }, [url]);
     
     return [data];
