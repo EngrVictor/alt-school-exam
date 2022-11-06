aaf8d67ab1d82a0ac623977300795f77fffb31e0
Okpara Victor Junior <52351658+EngrVictor@users.noreply.github.com>
2022-11-06 23:51:05 +0100

    Update Notifications.jsx

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import useFetch from './hooks/useFetch'
 
 const Notifications = () => {
-   const [data] = useFetch('https://api.github.com/users/engrvictor/repos')
-    console.log(data)
   
   return (
     <div><h1>Nothing to see here (' v ')</h1></div>
